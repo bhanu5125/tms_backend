c41cef07a19cda7a024bfd3fa81289ade80b08e6
Bhanu Prakash <kbhanu5125@gmail.com>
2025-05-20 19:16:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -192,7 +192,7 @@ app.post("/api/submit-form", (req, res) => {
     staff.Address || null,
     staff.PrimaryPhone,
     staff.SecondaryPhone || null,
-    staff.StaffType === 0 ? null : staff.StaffType
+    staff.StaffType === 0 ? null : staff.StaffType,
     staff.DeptId,
     1, // CreatedBy
     1, // ModifiedBy
